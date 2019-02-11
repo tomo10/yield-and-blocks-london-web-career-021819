ed455dc3843966fa02ed38b8155626dbc04bd521
Thomas Edwards <thce28@gmail.com>
2019-02-11 11:46:29 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+  if block_given?
   i = 0
   
   while i < array.length
@@ -7,6 +8,9 @@ def hello_t(array)
   end
   
   array
+else
+  puts "Hey! No block was given!"
 end
+end 
 
 
